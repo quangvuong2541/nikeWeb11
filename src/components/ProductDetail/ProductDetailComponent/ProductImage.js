0a76c55998628452d0631ec5341c5571507040d0
quangvuong2541 <truongquangvuong2000@gmail.com>
2022-10-12 21:15:15 +0700

    detail

--- src/components/ProductDetail/ProductDetailComponent/ProductImage.js
+++ src/components/ProductDetail/ProductDetailComponent/ProductImage.js
@@ -1,6 +1,37 @@
 import React from 'react'
+import { makeStyles } from '@mui/styles'
+import { Card, CardActionArea, CardMedia, Grid, Hidden, Skeleton } from '@mui/material'
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Container } from '@mui/system';
+import { useDispatch, useSelector } from 'react-redux';
 
-const ProductImage = () => {
+
+const useStyles = makeStyles((theme) => ({
+  ProductContainer :{
+      
+  },
+  ProductImage :{
+
+  },
+  image :{
+
+  }
+}))
+
+
+
+
+const ProductImage = ({detailProduct,index}) => {
+  const classes = useStyles()
+  const setting = {
+    dots: false,
+    Infinity: false,
+    autoplay: false,
+    slidesToScroll: 1,
+    slidesToShow: 1
+  }
   return (
     <div>ProductImage</div>
   )
