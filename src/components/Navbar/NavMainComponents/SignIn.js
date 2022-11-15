a83fcedf3eb231e5cc96a3b81c6f728244924993
quangvuong2541 <truongquangvuong2000@gmail.com>
2022-11-15 22:20:18 +0700

    sigin

--- src/components/Navbar/NavMainComponents/SignIn.js
+++ src/components/Navbar/NavMainComponents/SignIn.js
@@ -1,8 +1,102 @@
 import React from 'react'
+import { Dialog, Icon, makeStyles, Slide } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close';
+import { IconButton } from '@mui/material';
+import { connect } from 'react-redux';
+import * as action from "./Redux/Actions/action"
+import { useForm } from 'react-hook-form';
+import SignUp from './SignUp';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import { LOGOUT } from './Redux/Contansts/contants';
+import { Link } from 'react-router-dom';
 
-const SignIn = () => {
+
+const preventDefault = (event) => event.preventDefault();
+const useStyles = makeStyles((theme) => ({
+  nav1Menu: {},
+  backdrop: {},
+  SignInContainer: {},
+  closeSignIn: {},
+  nike: {},
+  formHeader: {},
+}))
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction='up' ref={ref} {...props} />
+})
+const SignIn = (props) => {
+  const classes = useStyles()
+  let { open } = props;
+  let { openSU } = props;
+  let { isAdmin } = props;
+
+  const [userMenu, setUserMenu] = React.useState(false)
+
+  //  validation form
+  const { register, handSubmit, errors } = useForm({
+    mode: "onBlur"
+  })
+  const onSubmitSignIn = (data, e) => {
+
+  }
+  const onCloseSignIn = (data, e) => {
+
+  }
+  const onSubmitSignUp = () => {
+
+  }
+  let { userLocal } = props;
   return (
-    <div>SignIn</div>
+    <div>
+      {!userLocal && (
+        <div >
+          <span className={classes.nav1Menu}
+          >Join us</span>
+
+          <span className={classes.nav1Menu}
+            onClick={() => {
+              props.emitOpen(!props.open)
+            }}
+          >
+            Sign In
+          </span>
+          <Dialog open={open ? true : false}
+            TransitionComponent={Transition}
+            keepMounted
+            onClose={() => {
+              props.emitOpen(!props.open)
+              onCloseSignIn()
+            }}
+            classes={{
+              root: classes.backdrop
+            }}
+          >
+            <div className={classes.SignInContainer} >
+              <IconButton
+                className={classes.closeSignIn}
+                onClick={() => {
+                  props.emitOpen(!open)
+                  onCloseSignIn();
+                }}
+              >
+              </IconButton>
+              {/* header */}
+              <div >
+                <center >
+                  <img src="https://royaltoys.com.vn/wp-content/uploads/2022/10/logo.png"
+                    className={classes.nike}
+                  />
+                </center>
+              </div>
+              <div className={classes.formHeader}>
+                YOUR ACCOUNT FOR EVERYTHING NIKE
+              </div>
+            </div>
+          </Dialog>
+        </div>
+      )}
+
+
+    </div>
   )
 }
 
--- src/components/Navbar/Navsub.js
+++ src/components/Navbar/Navsub.js
@@ -3,23 +3,123 @@ import { makeStyles } from '@mui/styles'
 import AppBar from '@mui/material/AppBar';
 import { Toolbar, Typography } from '@mui/material';
 import SignIn from './NavMainComponents/SignIn';
- 
 
-const useStyles = makeStyles ((theme) => ({
-    nav :{},
-    title :{},
-    toolbar:{},
-    
+
+const useStyles = makeStyles((theme) => ({
+    nav: {
+        backgroundColor: "white !important",
+        color: "black !important",
+        paddingLeft: 36,
+        paddingTop: 38,
+        position: "relative",
+        boxShadow: "none",
+        fontFamily: '"Helvetica Neue",Helvetica,Arial,sans-serif',
+        zIndex: 1000
+    },
+    title: {
+        flexGrow: 1,
+    },
+    toolbar: {
+        padding: 0,
+        minHeight: 36,
+    },
+    linkJordan: {
+        padding: "0 12px",
+        height: 34,
+        display: "flex",
+        alignItems: "center"
+    },
+    jordan: {
+        height: 24,
+        width: 24,
+        "&:hover": {
+            opacity: 0.7
+        }
+    },
+    nav1: {
+        height: 34,
+        display: "flex",
+        alignItems: "center",
+        textAlign: "center"
+    },
+    nav1Menu: {
+        margin: "0 12px",
+        textDecoration: "none",
+        color: "black",
+        fontSize: 12,
+        "&:hover": {
+            color: "grey",
+        },
+        cursor: "pointer"
+
+    },
+    helpMenuContainer: {
+        padding: "24px 24px 24px 18px",
+        position: "absolute",
+        right: 18,
+        zIndex: 10,
+        width: 200,
+        fontSize: 14,
+        textAlign: "left",
+        backgroundColor: "white"
+    },
+    helpMenuHeader: {
+        padding: "4px 8px",
+        marginBottom: 12,
+        fontSize: 16,
+        cursor: "pointer",
+    },
+    helpMenuItem: {
+        color: "#757575",
+        padding: "4px 8px",
+        cursor: "pointer",
+        "&:hover": {
+            color: "black"
+        }
+
+    },
+
 }))
 const Navsub = () => {
     const classes = useStyles()
     const [helpMenu, setHelpMenu] = React.useState(false)
-     
-  return (
-    <AppBar>
-        
-    </AppBar>
-  )
+
+    return (
+        <AppBar className={classes.nav} id="navsub">
+            <Toolbar className={classes.toolbar} >
+                <a href='#a' ib="jordan" className={classes.linkJordan}>
+                    <img src="https://royaltoys.com.vn/wp-content/uploads/2022/10/logo.png"
+                        className={classes.jordan}
+                    />
+                </a>
+                <Typography variant='h6' className={classes.title} ></Typography>
+                <div className={classes.nav1}>
+                    <div onMouseOver={() => setHelpMenu(true)}
+                        onMouseLeave={() => setHelpMenu(false)}
+                    >
+                        <span className={classes.nav1Menu}>
+                            Help
+                        </span>
+                        {helpMenu &&
+                            <div className={classes.helpMenuContainer} >
+                                <div className={classes.helpMenuHeader}  >Help</div>
+                                <div className={classes.helpMenuItem} >Order Status</div>
+                                <div className={classes.helpMenuItem} >Dispatch and Delivery</div>
+                                <div className={classes.helpMenuItem} >Returns</div>
+                                <div className={classes.helpMenuItem} >Contact Us</div>
+                                <div className={classes.helpMenuItem} >Privacy Policy</div>
+                                <div className={classes.helpMenuItem} >Terms of Sale</div>
+                                <div className={classes.helpMenuItem} >Terms of  Use</div>
+                                <div className={classes.helpMenuItem} >Send us FeedBack</div>
+                            </div>
+                        }
+                    </div>
+                    <SignIn />
+                </div>
+            </Toolbar>
+
+        </AppBar>
+    )
 }
 
 export default Navsub
